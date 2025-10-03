a951120eb30a1f322a8a932a80ae9058bf52598b
Ivonapp <ivona.v.hellsten@hotmail.com>
2025-10-03 16:06:07 +0200

    header

---
+++ js/dark.js
@@ -0,0 +1,32 @@
+const darkmodeSwitch = document.querySelector('#darkmode-switch')
+const hasDarkmode = localStorage.getItem('darkmode')
+
+if(hasDarkmode == null) {
+
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        enableDarkMode()
+    }  else {
+        disableDarkMode()
+
+    }
+
+    }
+
+    darkmodeSwitch.addEventListener('change', () => {
+        if(darkmodeSwitch.checked) {
+        enableDarkMode()
+    } else{
+        disableDarkMode()
+    }
+})
+
+    function enableDarkMode () {
+    darkmodeSwitch.checked = true 
+    document.documentElement.classList.add('dark')
+
+    }
+    function disableDarkMode () {
+    darkmodeSwitch.checked = false
+    document.documentElement.classList.remove('dark')
+
+    }
